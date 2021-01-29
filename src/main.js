349919e01197b0c141f38aac0349ff870b29f0a0
CuteAegyo <chiuwng10@gmail.com>
2021-01-29 11:12:24 +0800

    lol

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import VueTextareaAutosize from 'vue-textarea-autosize';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+const SECRET = process.env.SECRET;
+
+Vue.use(VueTextareaAutosize);
 
 Vue.config.productionTip = false
 
+firebase.initializeApp(SECRET);
+
+//exports our db so we can use it elsewhere
+export const db = firebase.firestore();
+
 new Vue({
-  render: h => h(App),
+    vuetify,
+    render: h => h(App)
 }).$mount('#app')
